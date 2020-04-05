151fa1106c89cce2e286dd48be5174c4a19a0e9e
jiajiayao <jiajiayaoabc@qq.com>
2020-04-05 18:43:34 +0800

    增加习惯分数优化

--- main.js
+++ main.js
@@ -6,19 +6,32 @@
  */
 (function() {
 const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
-$('.Patternbtn-div').click()
+  $('.Patternbtn-div').click()
+
+  //设置时间30分钟
+  time = 10;
   const start = async function() {
     while (true) {
 	console.log('后台运行中')
 	
 	$('.el-dialog__wrapper,.dialog-test').css("display","none")
-	$('.v-modal').css("z-index","-1")
-	
+    $('.v-modal').css("z-index", "-1")
+      
+      //需要加速请删除注释
+      /*
       if (!/1\.5/.test($('.speedBox').attr('style'))) {
         console.log('提升到1.5倍速')
         $('.speedTab15').click()
       }
-
+      */
+      if(time>0){  
+          console.log(time)
+          time=time-2;  
+      }  
+      else {  
+        //无习惯分数请注释这行代码
+        window.location.href = 'http://www.mahaixiang.cn/';
+      }  
       if ($('.volumeBox').find('.passVolume').height() != 0) {
         console.log('静音')
         $('.volumeIcon').click()
@@ -35,6 +48,9 @@ $('.Patternbtn-div').click()
       }
       await sleep(2000)
     }
+    
   }
+
   start()
+ 
 }())
